e5ada591166ffc7d24faf83a07d761f62bbc328a
Vadym <minerking112256@gmail.com>
2024-11-05 14:33:53 +0200

    Update book list page

--- app/books/page.tsx
+++ app/books/page.tsx
@@ -1,5 +1,28 @@
 "use client";
 
+import { useEffect, useState } from "react";
+import Button from "antd/es/button/button";
+import { Books } from "../components/Books";
+import { getAllBooks } from "../services/Books";
+
 export default function BooksPage() {
-    return  <></>
+  const [books, setBooks] = useState<Book[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getBooks = async () => {
+      const books = await getAllBooks();
+      setLoading(false);
+      setBooks(books);
+    };
+    
+    getBooks();
+  }, []);
+  return (
+    <div>
+      <Button>Add book</Button>
+
+      <Books books={books} />
+    </div>
+  );
 }
